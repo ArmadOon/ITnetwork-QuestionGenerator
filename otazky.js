aa2c5a881480255ad4607851c3fa3f978b0dec4a
Martin Pluhar <pluhar.martin@outlook.com>
2022-06-16 12:53:00 +0200

    80%

--- otazky.js
+++ otazky.js
@@ -44,16 +44,29 @@ const webdesignAndBootstrap = [
 
 
 let nahodnaOtazka;
-function vyberOtazku(okruh){
+
+function vyberOtazku(okruh) {
     nahodnaOtazka = okruh[Math.floor(Math.random() * okruh.length)];
     return nahodnaOtazka;
 }
+
 const odpoved = document.getElementById("odpoved")
 const modalButton = document.getElementById("modalButton");
-modalButton.addEventListener("click",function (){
-    let okruh = parseInt(prompt("Prosím vyber okruh otázek. 1 = Základy, 2 = OOP. 3 = NodeJS 4 = WebDesign"))
-    if (okruh === 1){
+modalButton.addEventListener("click", function () {
+    let okruhPrompt = parseInt(prompt("Prosím vyber okruh otázek. 1 = Základy, 2 = OOP, 3 = NodeJS, 4 = WebDesign"))
+    if (okruhPrompt === 1) {
         vyberOtazku(zaklady);
+        odpoved.innerHTML = `${nahodnaOtazka}`;
+    } else if (okruhPrompt === 2) {
+        vyberOtazku(oop);
+        odpoved.innerHTML = `${nahodnaOtazka}`;
+    } else if (okruhPrompt === 3) {
+        vyberOtazku(nodeJS);
+        odpoved.innerHTML = `${nahodnaOtazka}`;
+    }else if (okruhPrompt === 4){
+        vyberOtazku(webdesignAndBootstrap);
         odpoved.innerHTML = `${nahodnaOtazka}`
+    } else {
+        odpoved.innerHTML = "Prosím vyber si otázku od 1 - 4."
     }
 })
